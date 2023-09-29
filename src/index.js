fae52c46563d4752b0108f7d9ecf483eb879faf2
Kibreab Aklilu <Akkeba37@gmail.com>
2023-09-29 20:44:42 +0300

    Adding the redux store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import store from "./store";
 import router from "./routes";
 import "./index.css";
 
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
